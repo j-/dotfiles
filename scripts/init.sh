61f379ec70e99b5d522df7683666fa06cac83d4d
j- <jh@live.com.au>
2015-04-12 00:15:49 +1000

    Added .gitconfig

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,11 +5,27 @@ if ! [ -f 'scripts/init.sh' ]; then
 	test "${SHLVL}" == "1" && return || exit 0;
 fi
 
-TOCOPY=(.bashrc .bash_profile .bash_aliases);
+# prompt user for name and email address
+if [ -z "$GLOBAL_USERNAME" ]; then
+	read -ep "Version control user name: " username;
+	export GLOBAL_USERNAME="$username"
+fi;
+if [ -z "$GLOBAL_USEREMAIL" ]; then
+	read -ep "Version control email: " useremail;
+	export GLOBAL_USEREMAIL="$useremail"
+fi;
+
+TOCOPY=(.bashrc .bash_profile .bash_aliases .gitconfig);
 for FILE in "${TOCOPY[@]}"; do
 	rm -f "$HOME/$FILE"
 	ln -s "$PWD/$FILE" "$HOME/$FILE"
 done
+
+# configure git
+if which git > /dev/null; then
+	git config --global user.name "$GLOBAL_USERNAME"
+	git config --global user.email "$GLOBAL_USEREMAIL"
+fi;
 
 # move bin/ into PATH
 mkdir -p ~/.path
