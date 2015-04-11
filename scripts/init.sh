9c3f75afbcf35a34d6c8bfa3598ef1a0c03924e9
j- <jh@live.com.au>
2015-04-11 18:00:11 +1000

    Added basic PWD test

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-DIR=$(dirname -- "$0")/..;
+if ! [ -f 'scripts/init.sh' ]; then
+	echo 'Please run from dotfiles directory';
+	test "${SHLVL}" == "1" && return || exit 0;
+fi
 
-rm -f "$HOME/.bashrc"       && ln -s "$DIR/.bashrc"       "$HOME/.bashrc"
-rm -f "$HOME/.bash_profile" && ln -s "$DIR/.bash_profile" "$HOME/.bash_profile"
-rm -f "$HOME/.bash_aliases" && ln -s "$DIR/.bash_aliases" "$HOME/.bash_aliases"
+rm -f "$HOME/.bashrc"       && ln -s "$PWD/.bashrc"       "$HOME/.bashrc"
+rm -f "$HOME/.bash_profile" && ln -s "$PWD/.bash_profile" "$HOME/.bash_profile"
+rm -f "$HOME/.bash_aliases" && ln -s "$PWD/.bash_aliases" "$HOME/.bash_aliases"
+
+source ~/.bashrc
