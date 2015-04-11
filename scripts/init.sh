870b2b753207a42e26ffe8f15ef21ce6b8cb2177
j- <jh@live.com.au>
2015-04-11 17:13:38 +1000

    Moved install script to scripts/init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 DIR=$(dirname $0)
 rm -f "$HOME/.bashrc"       && ln -s "$DIR/bashrc"       "$HOME/.bashrc"
 rm -f "$HOME/.bash_aliases" && ln -s "$DIR/bash_aliases" "$HOME/.bash_aliases"
