08aca8a4b361eeb6efc481e69797a25c2eed7ae0
j- <jh@live.com.au>
2015-04-11 20:44:44 +1000

    Oops, recursively remove old bin folder

--- scripts/init.sh
+++ scripts/init.sh
@@ -11,7 +11,7 @@ rm -f "$HOME/.bash_aliases" && ln -s "$PWD/.bash_aliases" "$HOME/.bash_aliases"
 
 # move bin/ into PATH
 mkdir -p ~/.path
-rm -f ~/.path/dotfiles_bin
+rm -rf ~/.path/dotfiles_bin
 ln -s "$PWD/bin" "$HOME/.path/dotfiles_bin"
 
 source ~/.bashrc
