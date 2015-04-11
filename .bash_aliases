1b3d86e97c7d2daf7b2f35d943def4d1280a174e
j- <jh@live.com.au>
2015-04-11 17:18:48 +1000

    Added dots to dotfiles

---
+++ .bash_aliases
@@ -0,0 +1,4 @@
+alias edit="$EDITOR $1"
+alias explore="explorer $1"
+
+alias ls="ls --color=auto"
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 DIR=$(dirname $0)
-rm -f "$HOME/.bashrc"       && ln -s "$DIR/bashrc"       "$HOME/.bashrc"
-rm -f "$HOME/.bash_aliases" && ln -s "$DIR/bash_aliases" "$HOME/.bash_aliases"
+rm -f "$HOME/.bashrc"       && ln -s "$DIR/../.bashrc"       "$HOME/.bashrc"
+rm -f "$HOME/.bash_aliases" && ln -s "$DIR/../.bash_aliases" "$HOME/.bash_aliases"
+
+source ~/.bashrc
