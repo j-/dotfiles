f287b3eaf7df8a8afdf01834f217df75e2a15501
j- <j@skeoh.com>
2016-02-12 20:47:32 +1100

    Add simple install script

---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Configure a new system
+
+# Ensure all paths are relative to this script
+cd $(dirname "$BASH_SOURCE")
+
+# Copy dotfiles from repo to home dir
+./copy-files.sh ../src "$HOME"
