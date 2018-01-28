86fa57cd458790a63ea1aeaf952e85637c8b0eb3
Jamie Hoeks <jamie.hoeks@nab.com.au>
2018-01-29 09:41:25 +1100

    Fix install script to allow bin files to be copied

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,14 @@
 # Ensure all paths are relative to this script
 HERE=$(dirname "${BASH_SOURCE}")
 
-# Copy dotfiles from repo to home dir
+# Copy dotfiles from repo to ~
 "${HERE}/copy-files.sh" "${HERE}/../src" "${HOME}"
+
+# Ensure bin dir exists
+mkdir -p "${HOME}/bin"
+
+# Copy scripts from repo to ~/bin
+"${HERE}/copy-files.sh" "${HERE}/../src/bin" "${HOME}/bin"
 
 echo 'Finished'
 echo 'Run `. ~/.bash_profile` to load profile now'
