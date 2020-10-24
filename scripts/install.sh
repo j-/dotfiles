5fa291b74efe2ab642b55c2ef2a5f25e9609f3b5
j- <j@skeoh.com>
2020-10-24 14:33:16 +1100

    Remove bin scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,11 +8,5 @@ HERE=$(dirname "${BASH_SOURCE}")
 # Copy dotfiles from repo to ~
 "${HERE}/copy-files.sh" "${HERE}/../src" "${HOME}"
 
-# Ensure bin dir exists
-mkdir -p "${HOME}/bin"
-
-# Copy scripts from repo to ~/bin
-"${HERE}/copy-files.sh" "${HERE}/../src/bin" "${HOME}/bin"
-
 echo 'Finished'
 echo 'Run `. ~/.bash_profile` to load profile now'
