a12d7a8c2e854a425f5748bdf0e840ef07bb3087
j- <j@skeoh.com>
2020-10-24 16:08:57 +1100

    Make install scripts aware of application presence

--- src/starship.sh
+++ src/starship.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+if [ -z "$(command -v starship)" ]; then
+  echo 'Starship not detected: https://starship.rs/'
+  exit 1
+fi
+
 mkdir -p ~/.config
 cp -i ./starship/starship.toml ~/.config
--- src/vscode.sh
+++ src/vscode.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+if [ -z "$(command -v code)" ]; then
+  echo 'VS Code not detected: https://code.visualstudio.com/'
+  exit 1
+fi
+
 # Windows
-[ -d "$APPDATA/Code/User" ] && cp -i ./vscode/settings.json "$APPDATA/Code/User"
+[ -n "$APPDATA" ] && \
+[ -d "$APPDATA/Code/User" ] && \
+cp -i ./vscode/settings.json "$APPDATA/Code/User"
 
 # Mac
-[ -d "$HOME/Library/Application Support/Code/User" ] && cp -i ./vscode/settings.json "$HOME/Library/Application Support/Code/User"
+[ -d "$HOME/Library/Application Support/Code/User" ] && \
+cp -i ./vscode/settings.json "$HOME/Library/Application Support/Code/User"
